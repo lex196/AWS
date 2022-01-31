11b59ab46a8eb7041f85d5988b55db89a1a6056d
lex196 <alexisc6666@gmail.com>
2022-01-31 11:36:46 -0500

    new

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-1_r7r0Bf0wh', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5goukuk6m14jn6ia0rjc6aggue', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hvn
+        region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
